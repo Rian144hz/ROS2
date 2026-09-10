89b4aacd5c0afde11011552d9da3fba7e742c198
rian144hz <rianmatheus669@gmail.com>
2026-09-10 15:52:40 -0300

    My first node in ros2, here i start a journey

---
+++ firstros2.py
@@ -0,0 +1,19 @@
+import rclpy
+from rclpy.node import Node
+
+def main(args=None):
+    
+    rclpy.init(args=args)
+
+    
+    node = Node('my_first_node')
+
+   
+    node.get_logger().info('Hello, ROS 2! this is my first node, im very happy')
+
+   
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
